c16d04bdecc52e0dd011e2a110e39ca8355821a6
Andrew Natoli <4162073+AndrewNatoli@users.noreply.github.com>
2019-09-24 12:27:11 -0400

    fix: Omit 12:00 AM stops from the schedule output as that's used as a placeholder for no stop.

--- src/config/schedule.ts
+++ src/config/schedule.ts
@@ -4,6 +4,7 @@ export const URL_INBOUND_SATURDAY = "http://wirelesscats.ridetransit.org/BusSche
 export const URL_OUTBOUND_SATURDAY = "http://wirelesscats.ridetransit.org/BusSchedules/Search/Schedule?routeCode=6750&routeDirection=Outbound&bookingId=82&routeType=Saturday"
 export const URL_INBOUND_SUNDAY = "http://wirelesscats.ridetransit.org/BusSchedules/Search/Schedule?routeCode=6750&routeDirection=Inbound&bookingId=82&routeType=Sunday"
 export const URL_OUTBOUND_SUNDAY = "http://wirelesscats.ridetransit.org/BusSchedules/Search/Schedule?routeCode=6750&routeDirection=Outbound&bookingId=82&routeType=Sunday"
+export const SCHEDULE_NULL_TIME = "12:00 AM";
 
 export default {
   URL_INBOUND_WEEKDAY,
@@ -11,6 +12,7 @@ export default {
   URL_INBOUND_SATURDAY,
   URL_OUTBOUND_SATURDAY,
   URL_INBOUND_SUNDAY,
-  URL_OUTBOUND_SUNDAY
+  URL_OUTBOUND_SUNDAY,
+  SCHEDULE_NULL_TIME
 }
 
--- src/lib/transformSchedules.ts
+++ src/lib/transformSchedules.ts
@@ -1,4 +1,5 @@
 import { mergeAll } from 'ramda';
+import { SCHEDULE_NULL_TIME } from '../config/schedule';
 import { LynxSchedule } from '../global/types/remote/LynxSchedule';
 import { ScheduleGroups } from './fetchAllSchedules';
 
@@ -8,28 +9,28 @@ export const transformSchedules = (scheduleData: any) => {
 
   let result = {};
   Object.keys(scheduleData).reduce((transformedData, key) => {
-
     const timeData = mergeAll(scheduleData[key]);
     const directionKeys = Object.keys(timeData);
-    
+
     result[key] = {};
 
     directionKeys.forEach((directionKey, index) => {
       result[key][directionKey] = timeData[directionKey].stationStops.map((timeTable, timeTableIndex) => {
         const station = timeData[directionKey].stationNames[timeTableIndex];
-        const stationStops = timeData[directionKey].stationStops[timeTableIndex];
+        const stationStops = timeData[directionKey].stationStops[timeTableIndex].filter(
+          (time) => time !== SCHEDULE_NULL_TIME
+        );
         return {
           station,
           stationStops
         };
       });
-
     });
 
     return result;
   }, {});
 
   return result;
-}
+};
 
 export default transformSchedules;
